d3732cdc4a0cad4d10a79e7ad1ac6e05c553a162
iri-wealth <irina_kainz@yahoo.com>
2023-11-10 13:43:05 +0100

    updated serializers for detail url for recipes

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -11,3 +11,10 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Model serializer for recipe detail objects."""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
